6f6586d64e530fe6fd531bfe9124b3f7d460bc8a
Weston McNamee <ghost.squadron@gmail.com>
2022-05-02 17:14:14 -0700

    fix: 🐛  use "all" for arch if on darwin

--- lib/utils.sh
+++ lib/utils.sh
@@ -5,6 +5,20 @@ get_os() {
 
 # get the cpu architecture
 get_arch() {
+  local os="${1}"
+
+  if [ -z "${os}" ]; then
+    echo "architecture depends on OS. multi-arch used for some platforms."
+    exit 1
+  fi
+
+  # newest releases bundle arm/amd architectures
+  # not currently worried about being able to install older versions at the moment, sorry!
+  if [ "${os}" == "darwin" ]; then
+    echo "all"
+    return
+  fi
+
   local -r arch=$(uname -m)
 
   case $arch in
